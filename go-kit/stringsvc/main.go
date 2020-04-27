a19f24520b45bb2a2531e7b018a81b3b24049329
Darragh McCarthy <darraghm@faceme.com>
2020-04-27 15:47:55 +1200

    Add logging middleware

--- go-kit/stringsvc/main.go
+++ go-kit/stringsvc/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
@@ -27,7 +28,11 @@ type countResponse struct {
 
 // Transports expose the service to the network. In this first example we utilize JSON over HTTP.
 func main() {
-	svc := stringService{}
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	var svc StringService
+	svc = stringService{}
+	svc = loggingMiddleware{logger, svc}
 
 	uppercaseHandler := httptransport.NewServer(
 		makeUppercaseEndpoint(svc),
@@ -43,5 +48,6 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	logger.Log("msg", "HTTP", "addr", ":8080")
+	logger.Log("err", http.ListenAndServe(":8080", nil))
 }
